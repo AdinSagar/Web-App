c3b5200342d9b7618b5ce9ffd3051110c0e2c571
Adin Sagar <163553009+AdinSagar@users.noreply.github.com>
2026-09-24 21:30:48 -0500

    Add canonical Windows install URL

--- src/lib/business-app.ts
+++ src/lib/business-app.ts
@@ -2,3 +2,4 @@ const BUSINESS_APP_ORIGIN = 'https://app.fahampesa.com'
 
 export const BUSINESS_LOGIN_URL = `${BUSINESS_APP_ORIGIN}/?intent=login`
 export const BUSINESS_SIGNUP_URL = `${BUSINESS_APP_ORIGIN}/?intent=signup`
+export const BUSINESS_INSTALL_URL = `${BUSINESS_APP_ORIGIN}/?intent=install`
