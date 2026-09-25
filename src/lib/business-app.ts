2f1c99ad62f8fdddeeefb936556676fcc2adcc05
Adin Sagar <163553009+AdinSagar@users.noreply.github.com>
2026-09-24 21:21:54 -0500

    Centralize business app authentication URLs

---
+++ src/lib/business-app.ts
@@ -0,0 +1,4 @@
+const BUSINESS_APP_ORIGIN = 'https://app.fahampesa.com'
+
+export const BUSINESS_LOGIN_URL = `${BUSINESS_APP_ORIGIN}/?intent=login`
+export const BUSINESS_SIGNUP_URL = `${BUSINESS_APP_ORIGIN}/?intent=signup`
